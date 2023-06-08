15fe3e9081349b8c2de4612f20a8ddb9025c7265
tuvshintor <tuvshintor0925@protonmail.com>
2023-06-08 20:16:13 +0800

    second

---
+++ zodiac.js
@@ -0,0 +1,7 @@
+function year123() {
+    const mounth = document.getElementById('mounth');
+    const day = document.getElementById('day')
+    if (day == mounth) {
+        console.log('yes')
+    }
+}
